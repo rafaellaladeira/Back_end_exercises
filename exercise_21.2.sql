b8650c575204ef2f94d9da9f7726eaa4d5109b7f
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-05-31 14:49:04 -0300

    add notes

---
+++ exercise_21.2.sql
@@ -0,0 +1,9 @@
+INNER JOIN ||||  LEFT JOIN e RIGHT JOIN |||||  SELF JOIN
+
+JOIN => Permite combinar registros de duas ou mais tabelas,através do relacionamento que uma tabela tem com a outra.
+Serve para criação de relatórios, de novas maneiras de exigir uma informação já cadastrada em uma tabela e adicionar a tabelas existentes.
+
+>> INNER JOIN
+
+
+UNIONS | SUBQUERIES | STORED | PROCEDURES/FUNCTIONS
