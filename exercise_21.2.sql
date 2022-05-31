c16ad860f1e3b3d52842d1962e646ee485554dc8
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-05-31 18:34:44 -0300

    add notes

--- exercise_21.2.sql
+++ exercise_21.2.sql
@@ -3,7 +3,18 @@ INNER JOIN ||||  LEFT JOIN e RIGHT JOIN |||||  SELF JOIN
 JOIN => Permite combinar registros de duas ou mais tabelas,através do relacionamento que uma tabela tem com a outra.
 Serve para criação de relatórios, de novas maneiras de exigir uma informação já cadastrada em uma tabela e adicionar a tabelas existentes.
 
->> INNER JOIN
+>> INNER JOIN: o INNER JOIN permite retornar todos os resultados em que a condição da cláusula ON for satisfeita. 
+
+SELECT t1.coluna, t2.coluna
+FROM tabela1 AS t1
+INNER JOIN tabela2 AS t2
+ON t1.coluna_em_comum = t2.coluna_em_comum;
 
 
+
+
+
+
+
+Bônus:
 UNIONS | SUBQUERIES | STORED | PROCEDURES/FUNCTIONS 
