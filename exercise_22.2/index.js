884c9dee6e08102ef9436ccd83c8415809ab3598
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-19 09:54:53 -0300

    add

--- exercise_22.2/index.js
+++ exercise_22.2/index.js
@@ -1,87 +1,87 @@
 const fs = require('fs').promises;
 
-// const getAllSimpsons = async () => {
-//     const response  = await fs.readFile('./simpsons.json', 'utf8')
-//     const simpsons = JSON.parse(response);
-//     const strings = simpsons.map(({id, name}) => `${id} - ${name}`);
+const getAllSimpsons = async () => {
+    const response  = await fs.readFile('./simpsons.json', 'utf8')
+    const simpsons = JSON.parse(response);
+    const strings = simpsons.map(({id, name}) => `${id} - ${name}`);
 
-//     strings.forEach((string) => console.log(string));
-// }
+    strings.forEach((string) => console.log(string));
+}
 
-// function main() {
-//     getAllSimpsons();
-// }
-// main();
-// //______________________________________________________________________________
+function main() {
+    getAllSimpsons();
+}
+main();
+//______________________________________________________________________________
 
-// const getSimpsonById = async (id2) => {
-//     const response = await fs.readFile('./simpsons.json', 'utf8');
-//     const simpsons = JSON.parse(response);
+const getSimpsonById = async (id2) => {
+    const response = await fs.readFile('./simpsons.json', 'utf8');
+    const simpsons = JSON.parse(response);
 
-//    const chosen = simpsons.filter(({id})=> id == id2);
+   const chosen = simpsons.filter(({id})=> id == id2);
 
-//    if(!chosen) throw new Error('id não encontrado');
+   if(!chosen) throw new Error('id não encontrado');
 
-//    return chosen
-// }
-
-
-// async function main(){
-//     const simpson = await getSimpsonById(1);
-//     console.log(simpson);
-// }
-// main();
-// //____________________________________________________________
-
-// const removingSimpsons = () => {
-//     const response = await fs.readFile('./simpsons.json', 'utf8');
-//     const simpson = JSON.parse(response);
-
-//     const newArray = simpson.filter(({id})=> id !== 10 && id !== 6);
-
-//     await fs.writeFile('./simpsons.json', JSON.stringify(newArray)); //writefile sobrepõe tudo
-// }
-
-// async function main() {
-//     removingSimpsons();
-// }
-//___________________________________________________________________________
-
-// const creatingNewFile = async() => {
-//     const response = await fs.readFile('./simpsons.json', 'utf8');
-//     const simpson = JSON.parse(response);
+   return chosen
+}
 
 
-//     const arrayId = [ 1, 2, 3 ,4];
-//     const newTeste = simpson.filter(({id})=> arrayId.includes(Number(id)));
+async function main(){
+    const simpson = await getSimpsonById(1);
+    console.log(simpson);
+}
+main();
+//____________________________________________________________
+
+const removingSimpsons = () => {
+    const response = await fs.readFile('./simpsons.json', 'utf8');
+    const simpson = JSON.parse(response);
+
+    const newArray = simpson.filter(({id})=> id !== 10 && id !== 6);
+
+    await fs.writeFile('./simpsons.json', JSON.stringify(newArray)); //writefile sobrepõe tudo
+}
+
+async function main() {
+    removingSimpsons();
+}
+___________________________________________________________________________
+
+const creatingNewFile = async() => {
+    const response = await fs.readFile('./simpsons.json', 'utf8');
+    const simpson = JSON.parse(response);
+
+
+    const arrayId = [ 1, 2, 3 ,4];
+    const newTeste = simpson.filter(({id})=> arrayId.includes(Number(id)));
     
-//     await fs.writeFile('./simpsonFamily.json', JSON.stringify(newTeste));
-// }
+    await fs.writeFile('./simpsonFamily.json', JSON.stringify(newTeste));
+}
 
-// async function main() {
-//    await creatingNewFile();
-// }
-// main();
+async function main() {
+   await creatingNewFile();
+}
+main();
 
-//__________________________________________________
+__________________________________________________
 
-// const putName = async () => {
-//     const response = await fs.readFile('./simpsonFamily.json', 'utf8');
-//     const simpson = JSON.parse(response);
+const putName = async () => {
+    const response = await fs.readFile('./simpsonFamily.json', 'utf8');
+    const simpson = JSON.parse(response);
 
-//     const nameNelson = {
-//         id: 5,
-//         name: 'Nelson Muntz'
-//     } 
-//     simpson.push(nameNelson);
+    const nameNelson = {
+        id: 5,
+        name: 'Nelson Muntz'
+    } 
+    simpson.push(nameNelson);
 
-//     await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpson))
-// }
+    await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpson))
+}
 
-// async function main() {
-//     await putName();
-// }
-// main();
+async function main() {
+    await putName();
+}
+main();
 
 //_______________________________________________________
 
