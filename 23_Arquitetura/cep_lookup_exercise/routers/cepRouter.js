bf16a7351828c4770114c1b597d101ad5b59ec1d
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-06-28 20:21:52 -0300

    Encerrando esse exercicio pq bagunçou tudo

--- 23_Arquitetura/cep_lookup_exercise/routers/cepRouter.js
+++ 23_Arquitetura/cep_lookup_exercise/routers/cepRouter.js
@@ -3,6 +3,12 @@ const { Router } = require('express');
 const cepRouter = Router();
 
 cepRouter.get('/:cep', (req, res) => {
+    const { cep } = req.params;
+    try{
+        return 
+    } catch(error) {
+
+    }
     
 })
 
